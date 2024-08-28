29a1e8e3632de7cb1486328d800387a453f678b9
arsdev <senjka4@gmail.com>
2024-08-28 15:10:25 +0300

    Fixed checking if events are the same

--- src/oblenergo.ts
+++ src/oblenergo.ts
@@ -81,8 +81,7 @@ export async function getScheduleInfo(accountNumber: string | number): Promise<D
     let oblenergoResponse = await fetchOblengergoData(accountNumber)
     let result: DaySchedule[] = []
     const today = oblenergoResponse.graphs.today;
-    const todayMoment = tz.tz(timezone)
-    ;
+    const todayMoment = tz.tz(timezone);
     if(today != null) {
         result.push({
             events: parseOblenergoToScheduleEvent(today),
